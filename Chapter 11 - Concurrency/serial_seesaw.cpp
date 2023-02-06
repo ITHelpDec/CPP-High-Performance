69090d88bfa0bf94e7534c6413adcb29a0cf3d93
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-02-06 09:23:39 +0000

    Update serial_seesaw.cpp

--- Chapter 11 - Concurrency/serial_seesaw.cpp
+++ Chapter 11 - Concurrency/serial_seesaw.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
 
+void increase(std::size_t &x) {
+    for (int i = 0; i != 100; ++i) { ++x; }
+}
+
+void decrease(std::size_t &x) {
+    for (int i = 0; i != 100; ++i) { --x; }
+}
+
+int main()
+{
+    std::size_t x = 0;
+
+    increase(x);
+    decrease(x);
+
+    // x will be 0
+    std::cout << x << '\n';
+
+    return 0;
+}
