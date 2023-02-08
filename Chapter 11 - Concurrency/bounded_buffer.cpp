b4cd2b6cb16f637c3972f1f39c4e054ab7fa63c0
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2023-02-08 15:38:17 +0000

    Update bounded_buffer.cpp

--- Chapter 11 - Concurrency/bounded_buffer.cpp
+++ Chapter 11 - Concurrency/bounded_buffer.cpp
@@ -1,1 +1,20 @@
+#include <array>
+#include <iostream>
+#include <mutex>
 
+template <typename T, int N>
+class BoundedBuffer {
+
+private:
+    std::mutex mtx_;
+
+    std::array<T, N> buffer_;
+
+    std::size_t read_pos_, write_pos_;
+
+    void do_pushT&& item)
+    {
+        std::unique_lock lock(mtx_);
+        buffer_[write_pos_] = 
+    }
+};
